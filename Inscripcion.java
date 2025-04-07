8823199b0ec3a6b11429202b157ac50e9924a070
Osmar <osilveroruiz@frba.utn.edu.ar>
2025-04-07 15:00:43 -0300

    Update Inscripcion.java

--- Inscripcion.java
+++ Inscripcion.java
@@ -1,10 +1,6 @@
 package org.example;
 
-import org.example.Materia;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Collections;
-import java.util.stream.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,24 +9,6 @@ import lombok.Setter;
 
 public class Inscripcion{
 
-    public void inscribirAlumnoAMateria(ArrayList<Materia> materias, Alumno alumno){
-        if(this.puedeInscribirse(materias,alumno))
-        {
-            alumno.setInscripciones(materias);
-        };
-    }
-
-    public boolean aprobada(Materia materia, Alumno alumno ){
-      return alumno.aprobadas.contains(materia);
-    }
-
-    public boolean tieneAprobadaCorrelativas(Materia materia, Alumno alumno){
-        return alumno.aprobadas.containsAll(materia.correlativas);
-    }
-
-    public boolean puedeInscribirse(ArrayList<Materia> materias, Alumno alumno ){
-        return materias.stream().allMatch(materia -> this.tieneAprobadaCorrelativas(materia, alumno));
-    }
 
 
 }
